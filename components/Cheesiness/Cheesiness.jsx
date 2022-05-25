9d698d752336decaac9ec6d9722600944d3dd26a
Jake Pierantozzi <jakepierantozzi@gmail.com>
2022-05-25 01:36:49 -0400

    Update Cheesiness.jsx

--- components/Cheesiness/Cheesiness.jsx
+++ components/Cheesiness/Cheesiness.jsx
@@ -28,7 +28,7 @@ const Cheesiness = () => {
                 height={400}
               />
               <Typography variant="body1" className="description">
-                Jerri - Project Manager and Developer
+                Jerri
               </Typography>
             </Grid>
             <Grid item md={4} lg={4} sm={12} xs={12} className="hidden-mobile">
@@ -47,7 +47,7 @@ const Cheesiness = () => {
                 height={400}
               />
               <Typography variant="body1" className="description">
-                CJ - Marketing mouse
+                Lex
               </Typography>
             </Grid>
           </Grid>
@@ -61,7 +61,7 @@ const Cheesiness = () => {
                 height={400}
               />
               <Typography variant="body1" className="description">
-                TECHIES - Marketing mouse
+                Tuna
               </Typography>
             </Grid>
             <Grid item md={4} lg={4} sm={12} xs={12}>
@@ -77,71 +77,21 @@ const Cheesiness = () => {
             </Grid>
             <Grid item md={4} lg={4} sm={12} xs={12}>
               <Image
-                src="/assets/pfp-03.png"
-                alt="mouse"
-                width={400}
-                height={400}
-              />
-              <Typography variant="body1" className="description">
-                PHILIPPO - Frontend mouse
-              </Typography>
-            </Grid>
-          </Grid>
-          {/* 3rd row */}
-          <Grid
-            container
-            justifyContent="center"
-            alignItems="center"
-            alignContent="center"
-            spacing={3}
-            item
-            md={10}
-            lg={10}
-            sm={12}
-            xs={12}
-          >
-            <Grid item md={4} lg={4} sm={12} xs={12}>
-              <Image
                 src="/assets/pfp-02.png"
                 alt="mouse"
                 width={400}
                 height={400}
               />
+              <Typography variant="body1" className="description">
+                Mouse Artack
+              </Typography>
             </Grid>
           </Grid>
+          {/* 3rd row */}
+
 
           {/* 3rd row */}
-          <Grid
-            container
-            justifyContent="center"
-            alignItems="center"
-            alignContent="center"
-            item
-            md={10}
-            lg={10}
-            sm={12}
-            xs={12}
-            style={{padding: 0}}
-            className="description_wrapper"
-          >
-            <Typography
-              variant="h6"
-              className="description_title"
-            >
-              MOUSE ARTACK
-            </Typography>
-            <Typography variant="body1" className="description">
-              With a background in Fine arts and Design, our artist is mainly a
-              passionette girl with restless creative mind & passion for great
-              art. She has worked on numerous cool things like Branding/Visual
-              Identity, Digital art, Editorial/Publishing, Advertising &
-              Marketing Design and many more. In her free time she paints,
-              driven by the urge to reflect her emotions. Her artistic
-              inspiration is her everyday life, music, nature, animals and
-              people. Art is her greatest enjoyment and through her creations
-              she frees herself and lives in her own reality.
-            </Typography>
-          </Grid>
+
         </Grid>
       </div>
     </Container>
